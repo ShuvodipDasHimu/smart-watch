5c0a30772f18579e7d9b58b27a5f728c9e71b6d3
Shuvodip Das Himu <shuvodipdashimu@gmail.com>
2025-03-01 07:55:40 +0600

    final commit for this one..didnt do homework tho lol.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 const ringButtons = document.querySelectorAll(".ring-button");
-
+let productImageBase = "../images/"
 for(let i = 0; i < ringButtons.length; i++){
   const ringBtn = ringButtons[i];
   ringBtn.addEventListener("click", function(event){
@@ -16,7 +16,7 @@ for(let i = 0; i < ringButtons.length; i++){
     event.target.classList.remove("border-gray-300");
 
     const productImage = document.getElementById("product-image");
-    productImage.src = '../images/' + color + ".png";
+    productImage.src = productImageBase + color + ".png";
   });
 }
 
@@ -49,18 +49,78 @@ for(let btn of quantityElements){
 
 // add to cart
 let cartCount = 0;
+let cartItems = [];
 document.getElementById('add-to-cart').addEventListener('click', function(){
 
   const quantity = parseInt(document.getElementById("quantity").innerText);
   if(quantity > 0) {
     document.getElementById('checkout-container').classList.remove("hidden");
     cartCount = cartCount + quantity;
-
     document.getElementById('cart-count').innerText = cartCount;
+
+    const selectedColorButton = document.querySelector('button.border-purple-600.w-6');
+    const selectedColor = selectedColorButton.id.split("-")[0];
+  
+    const selectedSizeButtons = document.querySelector('button.border-purple-600:not(.w-6)'); // ARRGH???!!
+
+    const selectedSize = selectedSizeButtons.innerText.split(' ')[0];
+
+    const selectedPrice = selectedSizeButtons.innerText.split(' ')[1].split("$")[1];
+
+    const productImage = document.getElementById("product-image");
+
+
+    cartItems.push(
+      {
+        color: selectedColor,
+        image: selectedColor + ".png",
+        title: "Classy Modern Smart Watch",
+        size: selectedSize,
+        quantity: quantity,
+        price: quantity * parseInt(selectedPrice),
+      }
+    )
+    console.log(cartItems);
   }
   else{
-    alert("Please select a quantity!");
+    alert("Please select a quantity!!!");
+  }
+});
+
+document.getElementById("checkout-btn").addEventListener('click', function(){
+
+  const cartModal = document.getElementById("cart-modal");
+
+  const cartContainer = document.getElementById('cart-items');
+
+  for(let i = 0; i < cartItems.length; i++){
+
+    const item = cartItems[i];
+    const row = document.createElement("tr");
+    row.classList.add("border-b");
+    row.innerHTML = `
+    <td class="py-2>
+      <div class="flex items-center space-x-2">
+        <img class="h-12 w-12 object-cover rounded-md" src="${productImageBase}${item.image}" alt"">
+        <span class="font-semibold">${item.title}</span>
+      </div>
+    </td>
+    <td class="py-2 px-4">${item.color}</td>
+    <td class="py-2 px-4">${item.size}</td>
+    <td class="py-2 px-4">${item.quantity}</td>
+    <td class="py-2 px-4">$${item.price}</td>
+    `; // Total Homework
+    cartContainer.appendChild(row);
   }
 
 
+  cartModal.classList.remove('hidden');
+})
+
+document.getElementById('continue-shopping').addEventListener('click', function(){
+  document.getElementById("cart-modal").classList.add('hidden');
+})
+
+document.getElementById("checkout").addEventListener('click', function(){
+  alert("Continue to payment.");
 })
